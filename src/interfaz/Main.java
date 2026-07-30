c23085eebda3dc2ee17f47f52cc0c45064b3a6b7
darkyagami07 <mr.edward.jose07@gmail.com>
2026-07-30 08:02:49 -0400

    Estructura inicial del proyecto y clases base (Modelo, Datos, Reportes)
    Creación de paquetes (modelo, datos, reportes, interfaz) con la implementación inicial de Pelicula, Usuario, GestorCatalogo (Singleton), GestorPersistencia (Append CSV) y EstadisticasServicio.

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -1,7 +1,27 @@
 package interfaz;
 
+import datos.GestorCatalogo;
+import datos.GestorPersistencia;
+import modelo.Usuario;
+import reportes.EstadisticasServicio;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡CineBot 2026 listo para la Expoferia!");
+        System.out.println("=== INICIALIZANDO CINEBOT ===");
+
+        // 1. Cargar Catálogo (Singleton)
+        GestorCatalogo catalogo = GestorCatalogo.getInstancia();
+
+        // 2. Simulamos registro de prueba con Try-Catch blindado
+        try {
+            Usuario u1 = new Usuario("Edward", "Masculino", 2004);
+            u1.setPeliculaRecomendada("The Office");
+            GestorPersistencia.registrarUsuario(u1);
+        } catch (Exception e) {
+            System.err.println("Error no controlado: " + e.getMessage());
+        }
+
+        // 3. Mostrar Reportes Estadísticos
+        EstadisticasServicio.mostrarReporteGeneral();
     }
 }
